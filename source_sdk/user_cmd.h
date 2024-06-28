8523e6649d518f129fdf81748c63631aac9482a5
faluthe <faluninja@gmail.com>
2024-06-28 19:26:14 -0400

    created user_cmd struct

--- source_sdk/user_cmd.h
+++ source_sdk/user_cmd.h
@@ -1,4 +1,17 @@
+#ifndef USER_CMD_H
+#define USER_CMD_H
+#include "math/vec3.h"
+
 struct user_cmd
 {
-
+    void *something;
+    int command_number;
+    int tick_count;
+    struct vec3_t viewangles;
+    float forwardmove;
+    float sidemove;
+    float upmove;
+    int buttons;
 };
+
+#endif // USER_CMD_H
