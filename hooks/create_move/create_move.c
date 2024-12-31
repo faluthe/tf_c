86bd3362674a584e577e348418814c47318f6436
LeaoMartelo <leaomartelo@gmail.com>
2024-12-31 19:08:34 -0300

    rage autostrafe kinda works but dosent feel right random push forward sometimes

--- hooks/create_move/create_move.c
+++ hooks/create_move/create_move.c
@@ -1,11 +1,12 @@
 #include "../../config/config.h"
-#include "../../utils/utils.h"
 #include "../../source_sdk/cvar/convar/convar.h"
 #include "../../source_sdk/cvar/cvar.h"
 #include "../../source_sdk/engine_client/engine_client.h"
 #include "../../source_sdk/entity/entity.h"
 #include "../../source_sdk/user_cmd.h"
+#include "../../utils/utils.h"
 #include "../paint_traverse/paint_traverse.h"
+
 #include "create_move.h"
 
 #include <math.h>
@@ -28,7 +29,7 @@ __int64_t create_move_hook(void *this, float sample_time, struct user_cmd *user_
         log_msg("CreateMove hooked!\n");
         hooked = true;
     }
-    
+
     if (!is_in_game())
     {
         return rc;
@@ -50,19 +51,19 @@ __int64_t create_move_hook(void *this, float sample_time, struct user_cmd *user_
     {
         set_thirdperson(localplayer, false);
     }
-    
+
     if (user_cmd->tick_count > 1)
     {
         clear_render_queue();
         aimbot(localplayer, user_cmd);
         bunny_hop(localplayer, user_cmd);
-        autostrafe(localplayer, user_cmd);
+        rage_autostrafe(localplayer, user_cmd);
     }
 
     if (silent_aim)
     {
         return false;
-    } 
+    }
     else
     {
         return rc;
--- hooks/create_move/create_move.h
+++ hooks/create_move/create_move.h
@@ -1,5 +1,7 @@
 #include "../../source_sdk/user_cmd.h"
 
+#include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 
 __int64_t create_move_hook(void *this, float sample_time, struct user_cmd *user_cmd);
@@ -10,7 +12,7 @@ void aimbot(void *localplayer, struct user_cmd *user_cmd);
 void hitscan_aimbot(void *localplayer, struct user_cmd *user_cmd);
 void projectile_aimbot(void *localplayer, struct user_cmd *user_cmd, int weapon_id);
 
-
 /* MOVEMENT */
 void bunny_hop(void *localplayer, struct user_cmd *user_cmd);
 void autostrafe(void *localplayer, struct user_cmd *user_cmd);
+void rage_autostrafe(void *localplayer, struct user_cmd *user_cmd);
